fb57c3738ba8ffeada6ab1f0304fad653197a58c
Chris Yates <chris.m.yates@outlook.com>
2023-04-19 15:22:50 -0400

    Change carousel images to divs with backgrounds
    Add meta description
    Add some alt attributes

--- src/components/jt-main-carousel.js
+++ src/components/jt-main-carousel.js
@@ -9,36 +9,28 @@ JtMainCarouselTemplate.innerHTML = `
                 margin-inline: -0.5rem;
                 z-index: 0 !important;
                 -webkit-mask-image: linear-gradient(90deg, rgba(0,0,0,.2) 0, #000 15%, #000 85%, rgba(0,0,0,.2));
-                overflow: hidden;
             }
             #carouselTrack{
-                width: max-content;
-                overflow: hidden;
+                width: fit-content;
             }
             #carousel{
-                display: flex;
-                flex-wrap: nowrap;
+                display: inline-flex;
                 gap: 16px;
-                min-height: 150px;
-                max-height: 350px;
+                min-height: 350px;
                 margin-block: 16px;
-                justify-content: center;
-                position: relative;
                 animation: carousel 60s linear 0s infinite;
             }
             .childImage{
                 border-radius: 16px;
                 min-width: 500px;
-                min-height: 150px;
-                object-fit: cover;
+                min-height: 350px;
+                background-position: center;
             }
             @media (min-width: 768px){
                 #carousel{
                     max-height: 400px;
                 }
-                .childImage{
-                    width: auto;
-                }
+              
             }
             @media (max-width: 1024px){
                 #carouselScaffold{
@@ -49,16 +41,16 @@ JtMainCarouselTemplate.innerHTML = `
         <div id="carouselScaffold">
             <div id='carouselTrack'>
                 <div id='carousel'>
-                    <img src="/assets/woman2.webp" class="childImage">
-                    <img src="/assets/woman1.webp" class="childImage">
-                    <img src="/assets/womenscut.webp" class="childImage">
-                    <img src="/assets/menscut.webp" class="childImage">
-                    <img src="/assets/tools.webp" class="childImage">                    
-                    <img src="/assets/woman2.webp" class="childImage">
-                    <img src="/assets/woman1.webp" class="childImage">
-                    <img src="/assets/womenscut.webp" class="childImage">
-                    <img src="/assets/menscut.webp" class="childImage">
-                    <img src="/assets/tools.webp" class="childImage">
+                    <div style=" background-image: url('/assets/woman2.webp'); " class="childImage"></div>
+                    <div style=" background-image: url('/assets/woman1.webp'); " class="childImage"></div>
+                    <div style=" background-image: url('/assets/womenscut.webp'); " class="childImage"></div>
+                    <div style=" background-image: url('/assets/menscut.webp'); " class="childImage"></div>
+                    <div style=" background-image: url('/assets/tools.webp'); " class="childImage"></div>                    
+                    <div style=" background-image: url('/assets/woman2.webp'); " class="childImage"></div>
+                    <div style=" background-image: url('/assets/woman1.webp'); " class="childImage"></div>
+                    <div style=" background-image: url('/assets/womenscut.webp'); " class="childImage"></div>
+                    <div style=" background-image: url('/assets/menscut.webp'); " class="childImage"></div>
+                    <div style=" background-image: url('/assets/tools.webp'); " class="childImage"></div>
                 </div>
             </div>
         </div>
