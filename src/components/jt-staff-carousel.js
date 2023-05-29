d98e7c6aac8db2f8dc0db8a89c17c2ff7ef5852d
Chris Yates <chris.m.yates@outlook.com>
2023-05-29 16:50:52 -0400

    Prep for deploy
    Add/Remove staff
    Replace '~' with '+' in serviceList.js

--- src/components/jt-staff-carousel.js
+++ src/components/jt-staff-carousel.js
@@ -1,4 +1,5 @@
 import stylesheet from '../styles/index.css?inline'
+import {staff} from '../data/staff'
 const JTStaffCarouselTemplate = document.createElement('template');
 JTStaffCarouselTemplate.innerHTML = `
     <style>${stylesheet}</style>
@@ -10,37 +11,22 @@ JTStaffCarouselTemplate.innerHTML = `
             <img src="/assets/team/aimee.webp" height="300px" width="300px" class="rounded-full mx-auto opacity-40 transition-opacity select-none" draggable="false" alt="Aimee" id="staff-1" data-staffid="1"/>
         </div>
         <div class="carousel-item">
-            <img src="/assets/team/carol.webp" height="300px" width="300px" class="rounded-full mx-auto opacity-40 transition-opacity select-none" draggable="false" alt="Carol" id="staff-2" data-staffid="2"/>
+            <img src="/assets/team/kathy.webp" height="300px" width="300px" class="rounded-full mx-auto opacity-40 transition-opacity select-none" draggable="false" alt="Kathi" id="staff-2" data-staffid="2"/>
         </div>
         <div class="carousel-item">
-            <img src="/assets/team/dana.webp" height="300px" width="300px" class="rounded-full mx-auto opacity-40 transition-opacity select-none" draggable="false" alt="Dana" id="staff-3" data-staffid="3" loading="lazy" />
+            <img src="/assets/team/stephanie.webp" height="300px" width="300px" class="rounded-full mx-auto opacity-40 transition-opacity select-none" draggable="false" alt="Stephanie" id="staff-3" data-staffid="3" loading="lazy" />
         </div>
         <div class="carousel-item">
-            <img src="/assets/team/kathy.webp" height="300px" width="300px" class="rounded-full mx-auto opacity-40 transition-opacity select-none" draggable="false" alt="Kathy" id="staff-4" data-staffid="4" loading="lazy" />
-        </div>
-        <div class="carousel-item">
-            <img src="/assets/team/krista.webp" height="300px" width="300px" class="rounded-full mx-auto opacity-40 transition-opacity select-none" draggable="false" alt="Krista" id="staff-5" data-staffid="5" loading="lazy" />
-        </div>
-        <div class="carousel-item">
-            <img src="/assets/team/laura.webp" height="300px" width="300px" class="rounded-full mx-auto opacity-40 transition-opacity select-none" draggable="false" alt="Laura" id="staff-6" data-staffid="6" loading="lazy" />
-        </div>
-        <div class="carousel-item">
-            <img src="/assets/team/lauren.webp" height="300px" width="300px" class="rounded-full mx-auto opacity-40 transition-opacity select-none" draggable="false" alt="Lauren" id="staff-7" data-staffid="7" loading="lazy" />
-        </div>
-        <div class="carousel-item">
-            <img src="/assets/team/stephanie.webp" height="300px" width="300px" class="rounded-full mx-auto opacity-40 transition-opacity select-none" draggable="false" alt="Stephanie" id="staff-8" data-staffid="8" loading="lazy" />
+            <img src="/assets/team/valerie.webp" height="300px" width="300px" class="rounded-full mx-auto opacity-40 transition-opacity select-none" draggable="false" alt="Valerie" id="staff-4" data-staffid="4" loading="lazy" />
         </div>
         <div class="carousel-item" style="padding-right: calc((100% - 300px) / 2)">
-            <img src="/assets/team/valerie.webp" height="300px" width="300px" class="rounded-full mx-auto opacity-40 transition-opacity select-none" draggable="false" alt="Valerie" id="staff-9" data-staffid="9" loading="lazy" />
+            <img src="/assets/team/lisa.png" height="300px" width="300px" class="rounded-full mx-auto opacity-40 transition-opacity select-none" draggable="false" alt="Lisa" id="staff-5" data-staffid="5" loading="lazy" />
         </div>
     </div>
     <div id="staffBio" class="px-4 min-h-12 min-w-screen lg:max-w-5xl 2xl:max-w-7xl lg:mx-auto m-4">
         <div class='pt-4 !pb-12'>
-            <h1 class='font-bold text-center text-5xl lg:text-7xl text-black my-2'>FirstName <span class='jt-yellow'>LastName</span></h1>
-            <h2 class="text-center text-xl font-medium italic text-gray-500">Job Title</h2>
-            <p class="mt-2">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent maximus nibh vitae luctus sodales. Nunc sit amet libero in enim mollis mattis. Sed vel felis vitae ex ultricies malesuada. Maecenas fringilla porta euismod. Proin congue, odio et placerat euismod, dui nulla imperdiet orci, nec scelerisque lorem elit ac lectus. Mauris efficitur, dolor in mollis pretium, mauris augue ullamcorper dui, at sodales nisi metus faucibus metus. Ut dapibus quam est, quis accumsan ligula rhoncus ac.
-
-Pellentesque in nibh id nulla bibendum placerat ut sollicitudin eros. Nullam pellentesque justo non imperdiet sodales. Maecenas pulvinar turpis feugiat ullamcorper tristique. Quisque eu sodales tellus. Aenean mattis ipsum arcu, in sagittis nulla consequat in. Integer a ligula nec sapien malesuada lacinia vitae eu leo. Mauris blandit tellus ut volutpat viverra. Ut sit amet dapibus mi, a rutrum odio. Nunc urna odio, mattis suscipit eros id, ultrices pretium nibh. Nam efficitur nunc a felis suscipit, ultrices ultricies nisl eleifend. Suspendisse nunc magna, placerat vel fringilla ac, placerat at ligula. Maecenas in bibendum lacus. Duis varius sapien urna, eu placerat nulla lacinia ut. Etiam bibendum vel est sit amet tincidunt.</p>
+            <h1 class='font-bold text-center text-5xl lg:text-7xl text-black my-2'>Jody</h1>
+            <h2 class="text-center text-xl font-medium italic text-gray-500">Founder</h2>
         </div>
     </div>
 `
@@ -103,11 +89,8 @@ class JTStaffCarousel extends HTMLElement {
             this.staffBioRoot.innerHTML= "";
             this.staffBioRoot.innerHTML = `
                 <div class='pt-4 !pb-12'>
-                    <h1 class='font-bold text-center text-5xl lg:text-7xl text-black my-2'>FirstName <span class='jt-yellow'>LastName</span></h1>
-                    <h2 class="text-center text-xl font-medium italic text-gray-500">Job Title</h2>
-                    <p class="mt-2">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent maximus nibh vitae luctus sodales. Nunc sit amet libero in enim mollis mattis. Sed vel felis vitae ex ultricies malesuada. Maecenas fringilla porta euismod. Proin congue, odio et placerat euismod, dui nulla imperdiet orci, nec scelerisque lorem elit ac lectus. Mauris efficitur, dolor in mollis pretium, mauris augue ullamcorper dui, at sodales nisi metus faucibus metus. Ut dapibus quam est, quis accumsan ligula rhoncus ac.
-
-Pellentesque in nibh id nulla bibendum placerat ut sollicitudin eros. Nullam pellentesque justo non imperdiet sodales. Maecenas pulvinar turpis feugiat ullamcorper tristique. Quisque eu sodales tellus. Aenean mattis ipsum arcu, in sagittis nulla consequat in. Integer a ligula nec sapien malesuada lacinia vitae eu leo. Mauris blandit tellus ut volutpat viverra. Ut sit amet dapibus mi, a rutrum odio. Nunc urna odio, mattis suscipit eros id, ultrices pretium nibh. Nam efficitur nunc a felis suscipit, ultrices ultricies nisl eleifend. Suspendisse nunc magna, placerat vel fringilla ac, placerat at ligula. Maecenas in bibendum lacus. Duis varius sapien urna, eu placerat nulla lacinia ut. Etiam bibendum vel est sit amet tincidunt.</p>
+                    <h1 class='font-bold text-center text-5xl lg:text-7xl text-black my-2'>${staff[staffId].firstName}</h1>
+                    <h2 class="text-center text-xl font-medium italic text-gray-500">${staff[staffId].title}</h2>
                 </div>
             `;
             this.staffBioRoot.classList.remove('slide-out-bottom');
---
+++ src/data/staff.js
@@ -0,0 +1,32 @@
+export const staff = [
+        {
+            id: "0",
+            firstName: "Jody",
+            title: "Founder",
+        },
+        {
+            id: "1",
+            firstName: "Aimee",
+            title: "Stylist",
+        },
+        {
+            id: "2",
+            firstName: "Kathi",
+            title: "Stylist",
+        },
+        {
+            id: "3",
+            firstName: "Stephanie",
+            title: "Stylist",
+        },
+        {
+            id: "4",
+            firstName: "Valerie",
+            title: "Stylist",
+        },
+        {
+            id: "5",
+            firstName: "Lisa",
+            title: "Stylist",
+        },
+]
