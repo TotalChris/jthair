a11a4ca4ec17c8c79c0bc6cfcf4e16071fec7f0f
Chris Yates <chris@totalchris.com>
2023-10-03 11:31:11 -0400

    Move hours table into its own component
    Adjust margins for heroes on homepage

---
+++ src/components/jt-hours-table.js
@@ -0,0 +1,63 @@
+import {hours} from "../data/hours.js";
+import stylesheet from "../styles/index.css?inline"
+const JTHoursTableTemplate = document.createElement('template')
+JTHoursTableTemplate.innerHTML = `
+    <style>
+        ${stylesheet}
+    </style>
+    <table class="md:text-lg text-md">
+        <thead>
+            <tr>
+                <th></th>
+                <th></th>
+            </tr>
+        </thead>
+        <tbody>
+            <tr>
+                <td>Sunday</td>
+                <td class="openHour"></td>
+            </tr>
+            <tr>
+                <td>Monday</td>
+                <td class="openHour"></td>
+            </tr>
+            <tr>
+                <td>Tuesday</td>
+                <td class="openHour"></td>
+            </tr>
+            <tr>
+                <td>Wednesday</td>
+                <td class="openHour"></td>
+            </tr>
+            <tr>
+                <td>Thursday</td>
+                <td class="openHour"></td>
+            </tr>
+            <tr>
+                <td>Friday</td>
+                <td class="openHour"></td>
+            </tr>
+            <tr>
+                <td>Saturday</td>
+                <td class="openHour"></td>
+            </tr>
+        </tbody>
+    </table>
+`
+
+class JTHoursTable extends HTMLElement {
+
+    constructor() {
+        super();
+        this.attachShadow({mode: 'open'}).appendChild(JTHoursTableTemplate.content.cloneNode(true));
+    }
+
+    connectedCallback(){
+        this.shadowRoot.querySelectorAll('.openHour').forEach((entry, key) => {
+            let day = hours[key];
+            entry.innerHTML = (day.isOpen ? day.start + ' - ' + day.end : 'Closed')
+        })
+    }
+}
+
+window.customElements.define('jt-hours-table', JTHoursTable);
