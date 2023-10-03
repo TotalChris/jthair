b87845f569fd861daa1c5d6f050199e774a93c93
Chris Yates <chris@totalchris.com>
2023-10-03 14:17:08 -0400

    Change the way the staff carousel scrolls and displays names
    - Names now exist under each staff member. Names slide and fade in and when sliding out, fade out evenly.

--- src/components/jt-staff-carousel.js
+++ src/components/jt-staff-carousel.js
@@ -2,31 +2,61 @@ import stylesheet from '../styles/index.css?inline'
 import {staff} from '../data/staff'
 const JTStaffCarouselTemplate = document.createElement('template');
 JTStaffCarouselTemplate.innerHTML = `
-    <style>${stylesheet}</style>
-        <div class="carousel carousel-center mx-auto w-full gap-8 overflow-y-hidden cursor-grab select-none" id="staffCarousel" style="scroll-behavior: auto; height: 300px;">
-        <div class="carousel-item" style="padding-left: calc((100% - 300px) / 2);">
-            <img src="/assets/team/jody.webp" height="300px" width="300px" class="rounded-full mx-auto transition-opacity select-none" draggable="false" alt="Jody" id="staff-0" data-staffid="0" />
+    <style>
+        ${stylesheet}
+        .carousel-item{
+            display: flex;
+            flex-direction: column;
+        }
+        .bio {
+            opacity: 0;
+        }
+        #staff-0 .bio{
+            opacity: 1;
+        }
+    </style>
+        <div class="carousel carousel-center mx-auto w-full gap-8 overflow-y-hidden cursor-grab select-none" id="staffCarousel" style="scroll-behavior: smooth;">
+        <div class="carousel-item" id="staff-0" data-staffid="0" style="padding-left: calc((100% - 300px) / 2);">
+            <img src="/assets/team/jody.webp" height="300px" width="300px" class="rounded-full mx-auto transition-opacity select-none" draggable="false" alt="Jody" />
+            <div class='pt-4 !pb-12 bio'>
+                <h1 class='font-bold text-center text-4xl lg:text-5xl text-black my-2'>Jody</h1>
+                <h2 class="text-center text-xl font-medium italic text-gray-500">Stylist</h2>
+            </div>        
         </div>
-        <div class="carousel-item">
-            <img src="/assets/team/kathi.webp" height="300px" width="300px" class="rounded-full mx-auto opacity-40 transition-opacity select-none" draggable="false" alt="Kathi" id="staff-1" data-staffid="1"/>
+        <div class="carousel-item" id="staff-1" data-staffid="1">
+            <img src="/assets/team/kathi.webp" height="300px" width="300px" class="rounded-full mx-auto transition-opacity select-none opacity-40" draggable="false" alt="Kathi"/>
+            <div class='pt-4 !pb-12 bio'>
+                <h1 class='font-bold text-center text-4xl lg:text-5xl text-black my-2'>Kathi</h1>
+                <h2 class="text-center text-xl font-medium italic text-gray-500">Stylist</h2>
+            </div>
         </div>
-        <div class="carousel-item">
-            <img src="/assets/team/stephanie.webp" height="300px" width="300px" class="rounded-full mx-auto opacity-40 transition-opacity select-none" draggable="false" alt="Stephanie" id="staff-2" data-staffid="2" />
+        <div class="carousel-item" id="staff-2" data-staffid="2">
+            <img src="/assets/team/stephanie.webp" height="300px" width="300px" class="rounded-full mx-auto transition-opacity select-none opacity-40" draggable="false" alt="Stephanie" />
+            <div class='pt-4 !pb-12 bio'>
+                <h1 class='font-bold text-center text-4xl lg:text-5xl text-black my-2'>Stephanie</h1>
+                <h2 class="text-center text-xl font-medium italic text-gray-500">Stylist</h2>
+            </div>
         </div>
-        <div class="carousel-item">
-            <img src="/assets/team/valerie.webp" height="300px" width="300px" class="rounded-full mx-auto opacity-40 transition-opacity select-none" draggable="false" alt="Valerie" id="staff-3" data-staffid="3" loading="lazy" />
+        <div class="carousel-item" id="staff-3" data-staffid="3">
+            <img src="/assets/team/valerie.webp" height="300px" width="300px" class="rounded-full mx-auto transition-opacity select-none opacity-40" draggable="false" alt="Valerie" loading="lazy" />
+            <div class='pt-4 !pb-12 bio'>
+                <h1 class='font-bold text-center text-4xl lg:text-5xl text-black my-2'>Valerie</h1>
+                <h2 class="text-center text-xl font-medium italic text-gray-500">Stylist</h2>
+            </div>
         </div>
-        <div class="carousel-item">
-            <img src="/assets/team/ariel.webp" height="300px" width="300px" class="rounded-full mx-auto opacity-40 transition-opacity select-none" draggable="false" alt="Ariel" id="staff-4" data-staffid="4" loading="lazy" />
+        <div class="carousel-item" id="staff-4" data-staffid="4">
+            <img src="/assets/team/ariel.webp" height="300px" width="300px" class="rounded-full mx-auto transition-opacity select-none opacity-40" draggable="false" alt="Ariel" loading="lazy" />
+            <div class='pt-4 !pb-12 bio'>
+                <h1 class='font-bold text-center text-4xl lg:text-5xl text-black my-2'>Ariel</h1>
+                <h2 class="text-center text-xl font-medium italic text-gray-500">Stylist</h2>
+            </div>
         </div>
-        <div class="carousel-item" style="padding-right: calc((100% - 300px) / 2)">
-            <img src="/assets/team/lisa.png" height="300px" width="300px" class="rounded-full mx-auto opacity-40 transition-opacity select-none" draggable="false" alt="Lisa" id="staff-5" data-staffid="5" loading="lazy" />
-        </div>
-    </div>
-    <div id="staffBio" class="px-4 min-h-12 min-w-screen lg:max-w-5xl 2xl:max-w-7xl lg:mx-auto m-4">
-        <div class='pt-4 !pb-12'>
-            <h1 class='font-bold text-center text-5xl lg:text-7xl text-black my-2'>Jody</h1>
-            <h2 class="text-center text-xl font-medium italic text-gray-500">Stylist</h2>
+        <div class="carousel-item" id="staff-5" data-staffid="5" style="padding-right: calc((100% - 300px) / 2)">
+            <img src="/assets/team/lisa.png" height="300px" width="300px" class="rounded-full mx-auto transition-opacity select-none opacity-40" draggable="false" alt="Lisa" loading="lazy" />
+            <div class='pt-4 !pb-12 bio'>
+                <h1 class='font-bold text-center text-4xl lg:text-5xl text-black my-2'>Lisa</h1>
+                <h2 class="text-center text-xl font-medium italic text-gray-500">Stylist</h2>
+            </div>
         </div>
     </div>
 `
@@ -40,10 +70,12 @@ class JTStaffCarousel extends HTMLElement {
         this.scrollPos = 0
         this.pos = { left: 0, x: 0 };
         this.handleDrag = (e) => {
+            this.staffCarouselRoot.style.scrollBehavior = 'auto';
             const dx = e.clientX - this.pos.x;
             this.staffCarouselRoot.scrollLeft = this.pos.left - dx;
         }
         this.handleDragExit = (e) => {
+            this.staffCarouselRoot.style.scrollBehavior = 'smooth';
             this.staffCarouselRoot.style.scrollSnapType = "x mandatory"
             this.staffCarouselRoot.style.cursor = 'grab';
             this.staffCarouselRoot.removeEventListener('mousemove', this.handleDrag);
@@ -65,43 +97,24 @@ class JTStaffCarousel extends HTMLElement {
         })
     }
 
-    scrollToStaff(staffId){
-        this.staffCarouselRoot.scrollLeft = staffId * 332;
-        this.shadowRoot.querySelector('#staff-' + this.scrollPos).classList.add('opacity-40');
-        this.changeStaffFocus( {target: {scrollLeft: staffId * 332} } )
-    }
-
     changeStaffFocus(e){
         let s = (e.target.scrollLeft / 332);
+        this.scrollPos = s - (s % 1); //set the new queue position
+        this.shadowRoot.querySelectorAll('.carousel-item .bio').forEach((itm) => {itm.style.opacity = '0'})
+        this.shadowRoot.querySelectorAll('.carousel-item img').forEach((itm) => {itm.classList.add('opacity-40')})
         if(s % 1 === 0){
-            this.scrollPos = s - (s % 1);
-            this.shadowRoot.querySelector('#staff-' + this.scrollPos).classList.remove('opacity-40');
-            this.changeStaffDesc(this.scrollPos)
+            this.shadowRoot.querySelector('#staff-' + this.scrollPos + ' img').classList.remove('opacity-40')
+            this.shadowRoot.querySelector('#staff-' + this.scrollPos + ' .bio').style.opacity = '1'
         } else {
-            this.shadowRoot.querySelector('#staff-' + this.scrollPos).classList.add('opacity-40');
+            if(s < this.scrollPos){
+                this.shadowRoot.querySelector('#staff-' + (this.scrollPos - 1) + ' .bio').style.opacity = (1 - (s % 1)).toString()
+                this.shadowRoot.querySelector('#staff-' + this.scrollPos + ' .bio').style.opacity = (s % 1).toString()
+            } else if(s > this.scrollPos) {
+                this.shadowRoot.querySelector('#staff-' + this.scrollPos + ' .bio').style.opacity = (1 - (s % 1)).toString()
+                this.shadowRoot.querySelector('#staff-' + (this.scrollPos + 1) + ' .bio').style.opacity = (s % 1).toString()
+            }
         }
     }
-
-    changeStaffDesc(staffId){
-        this.staffBioRoot.classList.remove('slide-in-bottom');
-        this.staffBioRoot.classList.add('slide-out-bottom');
-        setTimeout(() => {
-            this.staffBioRoot.innerHTML= "";
-            this.staffBioRoot.innerHTML = `
-                <div class='pt-4 !pb-12'>
-                    <h1 class='font-bold text-center text-5xl lg:text-7xl text-black my-2'>${staff[staffId].firstName}</h1>
-                    <h2 class="text-center text-xl font-medium italic text-gray-500">${staff[staffId].title}</h2>
-                </div>
-            `;
-            this.staffBioRoot.classList.remove('slide-out-bottom');
-            this.staffBioRoot.classList.add('slide-in-bottom');
-        }, 500)
-    }
-
-    connectedCallback() {
-
-    }
-
 
 }
 
