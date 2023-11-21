233a934932ce2474d297aef669d223d23e3ea7f6
Chris Yates <chris@totalchris.com>
2023-11-21 03:29:42 -0500

    Add expand and contract effect on banner to JTServiceList
    - Also add automatic scroll-to-start when selection is made in a category

--- src/components/jt-service-list.js
+++ src/components/jt-service-list.js
@@ -21,13 +21,47 @@ JTServiceListTemplate.innerHTML = `
             background: url("/assets/icons/content/family.svg");
             background-size: contain;
         }
+        .buttonbar{
+        	position: absolute;
+    		right: 50%;
+    		transform: translate(50%, 0%);
+    		z-index: 30;
+    		transition: right .3s ease-in-out, transform .3s ease-in-out;
+        }
+        .header{
+        	opacity: 0; 
+        	transition: opacity .3s ease-in-out;
+        }
+        .expand .buttonbar{
+        	right: 0%;
+    		transform: translate(0%, 0%);
+        }
+        .expand .header{
+        	opacity: 100% !important;
+        }
+        @media (max-width: 768px) {
+        	.buttonbar{
+        		position: static;
+        		transform: none;
+        		right: 0;
+        	}
+        	.header{
+        		display: none;
+        	}
+        	#banner{
+        		justify-content: center;
+        	}
+        }
     </style>
-    <div class="pb-16 pt-6 !sticky top-32 z-10" style="background-image: linear-gradient(to bottom, white 55%, transparent 100%)">
-    <div class="bg-white mx-auto max-w-max flex h-12 sm:h-16 flex-row flex-nowrap gap-1 items-center justify-center tabs tabs-boxed bg-transparent border-black border-4 rounded-2xl !text-black ">
-        <div class="btn btn-ghost normal-case border-0 text-black text-md sm:text-lg flex flex-row grow !min-h-8 h-8 sm:h-full items-center gap-2 bg-jt-grad pl-2" id="womensBtn"><div class="m-icon woman"></div>Womens</div>
-        <div class="btn btn-ghost normal-case border-0 text-black text-md sm:text-lg flex flex-row grow !min-h-8 h-8 sm:h-full items-center gap-2 pl-2" id="mensBtn"><div class="m-icon man"></div>Mens</div>
-        <div class="btn btn-ghost normal-case border-0 text-black text-md sm:text-lg flex flex-row grow !min-h-8 h-8 sm:h-full items-center gap-2 pl-4" id="kidsBtn"><div class="m-icon family"></div>Kids</div>
-    </div>
+    <div id="banner" class="pb-16 pt-6 !sticky top-32 z-10 flex" style="background-image: linear-gradient(to bottom, white 55%, transparent 100%)">
+    	<h2 class="header font-bold text-center text-5xl text-black my-2">
+			Our <span class="bg-jt-grad-text">Services</span>
+		</h2>
+		<div class="buttonbar bg-white max-w-max flex h-12 sm:h-16 flex-row flex-nowrap gap-1 items-center justify-center tabs tabs-boxed bg-transparent border-black border-4 rounded-2xl !text-black ">
+			<div class="btn btn-ghost normal-case border-0 text-black text-md sm:text-lg flex flex-row grow !min-h-8 h-8 sm:h-full items-center gap-2 bg-jt-grad pl-2" id="womensBtn"><div class="m-icon woman"></div>Womens</div>
+			<div class="btn btn-ghost normal-case border-0 text-black text-md sm:text-lg flex flex-row grow !min-h-8 h-8 sm:h-full items-center gap-2 pl-2" id="mensBtn"><div class="m-icon man"></div>Mens</div>
+			<div class="btn btn-ghost normal-case border-0 text-black text-md sm:text-lg flex flex-row grow !min-h-8 h-8 sm:h-full items-center gap-2 pl-4" id="kidsBtn"><div class="m-icon family"></div>Kids</div>
+		</div>
     </div>
     <div id="serviceCardRoot" class="flex flex-col gap-2 mx-4"><jt-service-card name="On-The-Go Cut" price="$23.00" description="Cut and Shampoo" class="w-full _"></jt-service-card><jt-service-card name="Cut &amp; Blowout" price="$36.00" description="Cut, Shampoo, and Blowout" class="w-full _"></jt-service-card><jt-service-card name="All-Over Color" price="$69.00+" description="" class="w-full _"></jt-service-card><jt-service-card name="Full Highlights" price="$85.00+" description="" class="w-full _"></jt-service-card><jt-service-card name="Partial Highlights" price="$65.00+" description="" class="w-full _"></jt-service-card><jt-service-card name="Retouch" price="$60.00+" description="" class="w-full _"></jt-service-card><jt-service-card name="Balayage" price="$120.00+" description="" class="w-full _"></jt-service-card><jt-service-card name="Ombre" price="$120.00+" description="" class="w-full _"></jt-service-card><jt-service-card name="Shampoo &amp; Style" price="$30.00" description="" class="w-full _"></jt-service-card><jt-service-card name="Shampoo &amp; Roller Set" price="$42.00" description="" class="w-full _"></jt-service-card><jt-service-card name="Conditioning Treatment &amp; Blowout" price="$35.00+" description="" class="w-full _"></jt-service-card><jt-service-card name="Olaplex Hair Perfector &amp; Repair" price="$35.00" description="" class="w-full _"></jt-service-card><jt-service-card name="Malibu Treatment" price="$25.00" description="" class="w-full _"></jt-service-card><jt-service-card name="Fancy Do (Up Do)" price="$50.00+" description="" class="w-full _"></jt-service-card><jt-service-card name="Braiding" price="$20.00+" description="" class="w-full _"></jt-service-card><jt-service-card name="Perm" price="$65.00+" description="" class="w-full _"></jt-service-card><jt-service-card name="Eyebrow Waxing" price="$15.00" description="" class="w-full _"></jt-service-card><jt-service-card name="Lip Waxing" price="$8.00" description="" class="w-full _"></jt-service-card><jt-service-card name="Chin Waxing" price="$8.00" description="" class="w-full _"></jt-service-card></div>
     <h4 class="text-lg font-medium italic text-gray-400 my-8 text-left">All prices subject to change. Contact us for an accurate quote.</h4>
@@ -41,6 +75,7 @@ class JtServiceList extends HTMLElement {
 		);
 		this.serviceCardRoot = this.shadowRoot.querySelector("#serviceCardRoot");
 		this.category = 0;
+		this.serviceBanner = this.shadowRoot.querySelector("#banner");
 		this.womensButton = this.shadowRoot.querySelector("#womensBtn");
 		this.mensButton = this.shadowRoot.querySelector("#mensBtn");
 		this.kidsButton = this.shadowRoot.querySelector("#kidsBtn");
@@ -53,6 +88,14 @@ class JtServiceList extends HTMLElement {
 		this.kidsButton.addEventListener("click", () => {
 			this.setCategory(2);
 		});
+
+		this.shiftHeader = (e) => {
+			if(window.scrollY > 325){
+				this.serviceBanner.classList.add('expand')
+			} else {
+				this.serviceBanner.classList.remove('expand')
+			}
+		}
 	}
 
 	setCategory(category, force) {
@@ -117,11 +160,15 @@ class JtServiceList extends HTMLElement {
 						break;
 					}
 				}
+				window.scrollTo(0, 350);
 			},
 			force ? 0 : 500
 		);
 	}
-	connectedCallback() {}
+
+	connectedCallback(){
+		document.addEventListener('scroll', this.shiftHeader)
+	}
 }
 
 window.customElements.define("jt-service-list", JtServiceList);
