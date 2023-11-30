8d8560971b0dab4d1127a2fc791c79e72d5803ab
TotalChris <chris@chrisyates.dev>
2023-11-30 18:36:16 -0500

    December Hours Change

--- src/data/hours.js
+++ src/data/hours.js
@@ -2,17 +2,17 @@ export const hours = [
     {
         id: "0",
         day: "Sunday",
-        isOpen: false,
-        start: '0:00 AM',
-        end: '0:00 AM',
+        isOpen: true,
+        start: '10 AM',
+        end: '5 PM',
         restricted: false,
     },
     {
         id: "1",
         day: "Monday",
-        isOpen: false,
-        start: '0:00 AM',
-        end: '0:00 PM',
+        isOpen: true,
+        start: '10 AM',
+        end: '5 PM',
         restricted: false,
     },
     {
