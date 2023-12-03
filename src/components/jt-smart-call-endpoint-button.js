438e77de94fb10dad126a6ef3193a210cc3b7d20
TotalChris <chris@chrisyates.dev>
2023-12-03 14:29:50 -0500

    Change Text Size

--- src/components/jt-smart-call-endpoint-button.js
+++ src/components/jt-smart-call-endpoint-button.js
@@ -6,7 +6,7 @@ JTSmartCallEndpointButtonTemplate.innerHTML = `
     <style>
         ${stylesheet}
     </style>
-    ${(isMobile()?'<a href="tel:8105225028" class="btn btn-ghost text-black bg-jt-grad hover:bg-yellow-200 hover:cursor-pointer normal-case text-lg"><i class="bi bi-telephone"></i>&nbsp;&nbsp;':'<p class="text-black font-bold normal-case text-lg">')}
+    ${(isMobile()?'<a href="tel:8105225028" class="btn btn-ghost text-black bg-jt-grad hover:bg-yellow-200 hover:cursor-pointer normal-case text-lg"><i class="bi bi-telephone"></i>&nbsp;&nbsp;':'<p class="text-black font-bold normal-case text-xl">')}
     (810) 522-5028
     ${(isMobile()?'</a>':'</p>')}
 `
